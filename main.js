519d27d31d505839b177fd1c96370eeafddea9de
minhaz <minhaz.taher@gmail.com>
2020-03-07 10:19:13 +0600

    counting solve unsolve

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-let IssueCount = 0;
-let IssuSolved = 0;
 document
   .getElementById("issueInputForm")
   .addEventListener("submit", submitIssue);
+var issueNo = 0;
+var issueSolvedNo = 0;
 
 function submitIssue(e) {
   const getInputValue = id => document.getElementById(id).value;
@@ -21,7 +21,7 @@ function submitIssue(e) {
   localStorage.setItem("issues", JSON.stringify(issues));
 
   document.getElementById("issueInputForm").reset();
-  IssueCount++;
+  localStorage.setItem("issueNo", ++issueNo);
   fetchIssues();
   e.preventDefault();
 }
@@ -31,13 +31,13 @@ const setStatusClosed = id => {
 
   const currentIssue = issues.find(issues => issues.id === id.toString());
 
-  console.log(currentIssue);
-
   currentIssue.status = "Closed";
+
   currentIssue.description =
     "<strike>" + currentIssue.description + "</strike>";
   localStorage.setItem("issues", JSON.stringify(issues));
-  IssuSolved++;
+  localStorage.setItem("issueSolvedNo", ++issueSolvedNo);
+  // document.querySelector("#close" + id).disabled = true;
 
   fetchIssues();
 };
@@ -48,26 +48,63 @@ const deleteIssue = id => {
   const remainingIssues = issues.filter(issues => issues.id != id);
 
   localStorage.setItem("issues", JSON.stringify(remainingIssues));
+
+  if (localStorage.getItem("issueNo")) {
+    issueNo = parseInt(localStorage.getItem("issueNo"));
+    localStorage.setItem("issueNo", --issueNo);
+    localStorage.setItem("issueSolvedNo", --issueSolvedNo);
+  }
+
   fetchIssues();
 };
+
+// Btn Diasble
+
+// function btnDisable(id) {
+//   console.log(id);
+
+//   document.querySelector("#close" + id).className += "btn-danger";
+//   // document.querySelector("#close" + id).classList.remove("btn-warning");
+// }
 
 const fetchIssues = () => {
   const issues = JSON.parse(localStorage.getItem("issues"));
   const issuesList = document.getElementById("issuesList");
   issuesList.innerHTML = "";
 
-  for (var i = 0; i < issues.length; i++) {
+  for (var i = 0; i < issues.length && issues.length != null; i++) {
     const { id, description, severity, assignedTo, status } = issues[i];
+    console.log(status);
 
-    issuesList.innerHTML += `<div class="well">
+    if (status == "Closed") {
+      issuesList.innerHTML += `<div class="well">
                               <h6>Issue ID: ${id} </h6>
                               <p><span class="label label-info"> ${status} </span></p>
                               <h3> ${description} </h3>
                               <p><span class="glyphicon glyphicon-time"></span> ${severity}</p>
                               <p><span class="glyphicon glyphicon-user"></span> ${assignedTo}</p>
-                              <a href="#" onclick="setStatusClosed(${id})" class="btn btn-warning">Close</a>
+                              <a href="#" onclick="setStatusClosed(${id})" class="btn btn-warning" id="close${id}" disabled>Close</a>
                               <a href="#" onclick="deleteIssue(${id})" class="btn btn-danger">Delete</a>
                               </div>`;
+    } else {
+      issuesList.innerHTML += `<div class="well">
+                              <h6>Issue ID: ${id} </h6>
+                              <p><span class="label label-info"> ${status} </span></p>
+                              <h3> ${description} </h3>
+                              <p><span class="glyphicon glyphicon-time"></span> ${severity}</p>
+                              <p><span class="glyphicon glyphicon-user"></span> ${assignedTo}</p>
+                              <a href="#" onclick="setStatusClosed(${id})" class="btn btn-warning" id="close${id}">Close</a>
+                              <a href="#" onclick="deleteIssue(${id})" class="btn btn-danger">Delete</a>
+                              </div>`;
+    }
   }
-  console.log(IssueCount + " " + IssuSolved);
+  issueCount();
 };
+
+function issueCount() {
+  document.getElementById("IR").innerHTML = localStorage.getItem("issueNo");
+  document.getElementById("IS").innerHTML =
+    localStorage.getItem("issueNo") - localStorage.getItem("issueSolvedNo");
+}
+
+// <a href="#" onclick="setStatusClosed(${id})" class="btn btn-warning" id="close${id}">Close</a>
